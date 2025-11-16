c9f239ddbf63c7e68cd6d48d876ee675ca2456c4
Avadooth <avadoothjoshi2001@gmail.com>
2025-11-16 14:58:36 +0530

    Fixed login issue

--- Fe/src/App.jsx
+++ Fe/src/App.jsx
@@ -1,22 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/PrivateRoute";
 
-import './index.css';
+import "./index.css";
 
 function App() {
-
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<PrivateRoute>
-          <Dashboard />
-        </PrivateRoute>} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
--- Fe/src/pages/Signup.jsx
+++ Fe/src/pages/Signup.jsx
@@ -2,20 +2,29 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import API from "../api";
 import "../Style/SignIn.css";
+import { useForm } from "react-hook-form";
 
 export default function Signup() {
-  const [form, setForm] = useState({ name: "", email: "", password: "" });
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit: handleFormSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({
+    mode: "onBlur",
+  });
 
-  const handleSubmit = async (e) => {
+  const onsubmit = async (data) => {
     e.preventDefault();
     try {
-      const response = await API.post("/auth/signup", form);
+      const response = await API.post("/auth/signup", data);
       setShowModal(true);
-    } catch(e) {
+      reset();
+    } catch (e) {
       console.error("Signup error:", e);
-      alert("Signup failed",e.message); 
+      alert("Signup failed", e.message);
     }
   };
   const handleModalOk = () => {
@@ -30,7 +39,11 @@ export default function Signup() {
             <div className="signin-card">
               <h1 className="signin-title">Sign in</h1>
 
-              <form onSubmit={handleSubmit} className="signin-form" noValidate>
+              <form
+                onSubmit={handleFormSubmit(onsubmit)}
+                className="signin-form"
+                noValidate
+              >
                 {/* Name */}
                 <div className="form-field">
                   <label htmlFor="name" className="form-label">
@@ -41,11 +54,23 @@ export default function Signup() {
                     type="text"
                     required
                     placeholder="Enter your name"
-                    value={form.name}
-                    onChange={(e) => setForm({ ...form, name: e.target.value })}
+                   
+                    {...register("name", {
+                      required: "Name is required",
+                      minLength: {
+                        value: 2,
+                        message: "Name must be at least 2 characters",
+                      },
+                    })}
+                    // onChange={(e) => setForm({ ...form, name: e.target.value })}
                     className="form-input"
                     autoComplete="name"
                   />
+                  {errors.name && (
+                    <p className="field-error text-red-600">
+                      {errors.name.message}
+                    </p>
+                  )}
                 </div>
                 <div className="form-field">
                   <label htmlFor="email" className="form-label">
@@ -56,11 +81,25 @@ export default function Signup() {
                     type="email"
                     required
                     placeholder="name@company.com"
-                    value={form.email}
-                    onChange={(e) => setForm({ ...form, email: e.target.value })}
+                    
+                    {...register("email", {
+                      required: "Email is required",
+                      pattern: {
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: "Enter a valid email address",
+                      },
+                    })}
+                    // onChange={(e) =>
+                    //   setForm({ ...form, email: e.target.value })
+                    // }
                     className="form-input"
                     autoComplete="email"
                   />
+                  {errors.email && (
+                    <p className="field-error text-red-600">
+                      {errors.email.message}
+                    </p>
+                  )}
                 </div>
 
                 {/* Password */}
@@ -73,24 +112,37 @@ export default function Signup() {
                     type="password"
                     required
                     placeholder="••••••••"
-                    value={form.password}
-                    onChange={(e) =>
-                      setForm({ ...form, password: e.target.value })
-                    }
+                  
+                    // onChange={(e) =>
+                    //   setForm({ ...form, password: e.target.value })
+                    // }
+                    {...register("password", {
+                      required: "Password is required",
+                      minLength: {
+                        value: 6,
+                        message: "Password must be at least 6 characters",
+                      },
+                    })}
                     className="form-input"
                     autoComplete="current-password"
                   />
+                  {errors.password && (
+                    <p className="field-error text-red-600">
+                      {errors.password.message}
+                    </p>
+                  )}
                 </div>
-
-
 
                 {/* Submit */}
                 <div className="form-field submit-field">
-                  <button type="submit" className="submit-btn">
-                    Sign in
+                  <button
+                    type="submit"
+                    className="submit-btn"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? "Signing up..." : "Sign up"}
                   </button>
                 </div>
-
               </form>
             </div>
           </div>
@@ -109,7 +161,6 @@ export default function Signup() {
           </div>
         </div>
       )}
-
     </>
   );
 }
